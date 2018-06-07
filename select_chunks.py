0598bb4a86bbe0191fb7e9945b78b53de50f7c4c
Martino Mensio <martinomensio@outlook.it>
2018-06-07 16:52:56 +0100

    passing parameters around

--- select_chunks.py
+++ select_chunks.py
@@ -11,6 +11,7 @@ Rules: chunks are selected for the gold standard if:
 import csv
 import librosa
 import operator
+import plac
 
 from collections import defaultdict
 from pathlib import Path
@@ -18,20 +19,19 @@ from tqdm import tqdm
 
 import data
 
-MIN_DURATION = 3
-
 
 def read_csv_files(path):
     # for each chunk name, for each class count the votes
     counts = defaultdict(lambda: defaultdict(lambda: 0))
-    csv_files = path.glob('*.csv')
+    # just select taggers with name, there is an anonymous tagged.csv
+    csv_files = path.glob('tagged_*.csv')
     n_voters = 0
     for file_path in csv_files:
         n_voters += 1
         with file_path.open('r') as f:
             reader = csv.reader(f, delimiter=',')
             rows = [(l[0], l[1]) for l in reader]
-            # remove duplicate for same tagger
+            # remove duplicate for same tagger (does not remove same voter voting different classes)
             rows = set(rows)
             for row in rows:
                 chunk_name = row[0]
@@ -41,22 +41,23 @@ def read_csv_files(path):
     return counts, n_voters
 
 
-def filter_chunks(counts, n_voters):
+def filter_chunks(counts, n_voters, min_duration, required_count):
     print('initial #chunks:',len(counts.keys()))
-    counts = count_based_filter(counts, n_voters)
-    counts = duration_based_filter(counts)
+    counts = count_based_filter(counts, n_voters, required_count)
+    counts = duration_based_filter(counts, min_duration)
 
     return counts
 
 
-def count_based_filter(counts, n_voters):
+def count_based_filter(counts, n_voters, required_count=None):
     # criterion 1: no 'unknown' votes
     counts = {chunk_name: chunk_counts for (chunk_name, chunk_counts) in counts.items()
               if not chunk_counts['unknown']}
     print('#chunks after count-based criterion 1 (no unknown votes):',len(counts.keys()))
 
     # criterion 2: at least 2/3 of people agree
-    required_count = (n_voters * 2) // 3
+    if not required_count:
+        required_count = (n_voters * 2) // 3
     print('required_count:', required_count)
     counts = {chunk_name: chunk_counts for chunk_name, chunk_counts in counts.items()
               if any(class_count >= required_count for class_name, class_count in chunk_counts.items())}
@@ -65,9 +66,9 @@ def count_based_filter(counts, n_voters):
     return counts
 
 
-def duration_based_filter(counts):
+def duration_based_filter(counts, min_duration):
     results = {}
-    print('required minimum duration (seconds):', MIN_DURATION)
+    print('required minimum duration (seconds):', min_duration)
     # a list of all the .mp3 files
     chunk_files_locations = data.CHUNKS_LOCATION.glob('*.mp3')
     # select only the ones that match the previous selection
@@ -77,7 +78,7 @@ def duration_based_filter(counts):
         audio, sample_rate = librosa.load(str(chunk_location))
         duration = librosa.get_duration(audio, sample_rate)
         chunk_name = chunk_location.name
-        if duration > MIN_DURATION:
+        if duration > min_duration:
             results[chunk_name] = counts[chunk_name]
     print('#chunks after duration-based filter:', len(results.keys()))
     
@@ -97,9 +98,12 @@ def write_winners(winners, location):
         writer.writerows(winners)
 
 
-def main():
+@plac.annotations(
+    min_duration=plac.Annotation("The minimum duration in seconds", 'option', 'd', metavar='SEC', type=int),
+    required_count=plac.Annotation("How many voters have to agree, if None (default) is provided the value will be 2 of 3 of the number of voters", 'option', 'c', metavar='COUNT', type=int))
+def main(min_duration=3, required_count=None):
     counts, n_voters = read_csv_files(data.DOCS_LOCATION)
-    counts = filter_chunks(counts, n_voters)
+    counts = filter_chunks(counts, n_voters, min_duration, required_count)
     winners = select_winners(counts)
     counts = defaultdict(lambda: 0)
     for _, label in winners:
@@ -108,4 +112,4 @@ def main():
     write_winners(winners, data.GOLD_LOCATION)
 
 if __name__ == '__main__':
-    main()
+    plac.call(main)
